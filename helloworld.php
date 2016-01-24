4fef16688da91ce3a0f78a92b8c609220f84c410
kakaisaname <1198560793@qq.com>
2016-01-24 10:43:11 +0800

    将第二次修改的内容提交

--- helloworld.php
+++ helloworld.php
@@ -5,5 +5,5 @@ echo $a;
 echo $a."sdad";
 var_dump($a);
 
-$array = array('hello'=>'kaka','where'=>'you are','world'=>'i');
+$array = array('hello'=>'kaka','where'=>'you are','world'=>'i','chen'=>'love');
 var_dump($array);
